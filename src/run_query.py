45fd7369e246f458bd2a543d5da4e1c4c36a1c4c
Virginia <virginia.carmuega@gmail.com>
2026-07-27 23:01:00 +0000

    Implement basic customer support assistant flow

--- src/run_query.py
+++ src/run_query.py
@@ -1,15 +1,29 @@
 from openai_client import create_cliente
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+def load_prompt(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
 
 client = create_cliente()
 
-
+system_prompt = load_prompt(BASE_DIR / "prompts" / "main_prompt.md")
 
 completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages= [
             {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
                 "role": "user",
-                "content": "Hola, ¿quién sos?"
+                "content": "¿Cuanto tiempo tiene el cliente para arrepentirse de su compra?"
             }
         ]
     )
